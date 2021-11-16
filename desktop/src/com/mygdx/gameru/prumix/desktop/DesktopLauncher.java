fd2f3b0610b0338c5f324775cbdce99329784b67
Prumixxx <prumixxx@gmail.com>
2021-11-16 17:56:40 +0200

    Initial commit

---
+++ desktop/src/com/mygdx/gameru/prumix/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.gameru.prumix.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.gameru.prumix.StarFight;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarFight(), config);
+	}
+}
