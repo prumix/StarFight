b9b90a7cc315d73aa233531d2a3d54c43da5b10f
Prumixxx <prumixxx@gmail.com>
2021-11-24 10:56:57 +0200

    движение логотипа при нажатии клавиши мыши(исправлено)

--- core/src/com/mygdx/gameru/prumix/screen/impl/MenuScreen.java
+++ core/src/com/mygdx/gameru/prumix/screen/impl/MenuScreen.java
@@ -7,8 +7,11 @@ import com.mygdx.gameru.prumix.screen.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private final float V_LEN = 2f;
     private Texture img;
     private Vector2 touch;
+    private Vector2 pos;
+    private Vector2 v;
 
 
     @Override
@@ -16,14 +19,23 @@ public class MenuScreen extends BaseScreen {
         super.show();
         img = new Texture("badlogic.jpg");
         touch = new Vector2();
+        pos = new Vector2();
+        v = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-        batch.draw(img, touch.x, touch.y);
+        batch.draw(img, pos.x, pos.y);
         batch.end();
+
+        if (touch.dst(pos) >= V_LEN){
+            pos.add(v);
+        }
+        else {
+            pos.set(touch);
+        }
 
     }
 
@@ -36,6 +48,7 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
         return super.touchDown(screenX, screenY, pointer, button);
     }
 }
