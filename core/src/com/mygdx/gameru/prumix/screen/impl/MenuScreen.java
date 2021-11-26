a5f841e8056b54b7f20750dae12dd45e61cffb96
Prumixxx <prumixxx@gmail.com>
2021-11-26 10:51:32 +0200

    hw3

--- core/src/com/mygdx/gameru/prumix/screen/impl/MenuScreen.java
+++ core/src/com/mygdx/gameru/prumix/screen/impl/MenuScreen.java
@@ -1,53 +1,65 @@
 package com.mygdx.gameru.prumix.screen.impl;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.gameru.prumix.math.Rect;
 import com.mygdx.gameru.prumix.screen.BaseScreen;
 import com.mygdx.gameru.prumix.sprite.impl.Background;
+import com.mygdx.gameru.prumix.sprite.impl.Logo;
 
 public class MenuScreen extends BaseScreen {
 
-    private final float V_LEN = 2f;
-
     private Texture bg;
-    private Vector2 pos;
+    private Texture lg;
+
     private Background background;
+    private Logo logo;
 
 
     @Override
     public void show() {
         super.show();
+        lg = new Texture("badlogic.jpg");
         bg = new Texture("textures/bg.png");
-        pos = new Vector2();
         background = new Background(bg);
+        logo = new Logo(lg);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        batch.begin();
-        background.draw(batch);
-        batch.end();
+       update(delta);
+       draw();
 
     }
-
     @Override
     public void dispose() {
         super.dispose();
         bg.dispose();
+        lg.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        pos.set(touch);
-        return super.touchDown(touch, pointer, button);
+        logo.touchDown(touch,pointer,button);
+        return false;
+    }
+
+    private void update(float delta) {
+        logo.update(delta);
+    }
+
+    private void draw(){
+        batch.begin();
+        background.draw(batch);
+        logo.draw(batch);
+        batch.end();
     }
 }
